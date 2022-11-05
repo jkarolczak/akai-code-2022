8f8a4981138afcb88c4d17fbba0a6ab4108ad06a
jacek.karolczak <jacek.karolczak@brytlyt.com>
2022-11-05 02:26:08 +0100

    Add env and admin panel

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 from poi import POI
-from psql_conn import list_pois, list_food, get_food_by_poi, get_poi
+from psql_conn import list_pois, list_food, get_food_by_poi, get_poi, add_food_to_poi, get_food
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -25,8 +25,13 @@ def poi(idx: int):
 
 @app.route('/admin/<idx>', methods=["GET", "POST"])
 def admin(idx: int):
+    poi = POI(get_poi(idx))
     if request.method == "GET":
-        food_list = list_food
-        return render_template("admin_form.html", food_list=food_list)  # TODO: prepare template
+        food_list = list_food()
+        return render_template("admin_form.html", name=poi.name, food_list=food_list)  # TODO: prepare template
     elif request.method == "POST":
-        food_name = request.form.get("food_name")
+        food_id = request.form.get("product")
+        food_name = get_food(food_id)
+        quantity = request.form.get("quantity")
+        add_food_to_poi(food_id, poi.id, quantity)
+        return render_template("admin_submit.html", name=poi.name, product=food_name, quantity=quantity)
--- src/psql_conn.py
+++ src/psql_conn.py
@@ -45,3 +45,31 @@ def get_poi(poi_id: int):
         curs.execute(f"SELECT * FROM poi WHERE poi_id={poi_id};")
         poi = curs.fetchmany(size=1)[0]
     return poi
+
+
+def get_food(food_id: int):
+    conn = get_conn()
+    with conn.cursor() as curs:
+        curs.execute(f"SELECT name FROM food WHERE food_id={food_id};")
+        name = curs.fetchmany(size=1)[0]
+    return name[0]
+
+
+def add_food_to_poi(food_id: int, poi_id: int, quantity: int):
+    conn = get_conn()
+    with conn.cursor() as curs:
+        curs.execute("SELECT quantity FROM poi_food WHERE poi_id=%(poi_id)s AND food_id=%(food_id)s",
+                     {"poi_id": poi_id, "food_id": food_id})
+        cur_quantity = curs.fetchall()
+
+    cur_quantity = cur_quantity[0][0] if len(cur_quantity) else 0
+
+    with conn.cursor() as curs:
+        curs.execute("DELETE FROM poi_food WHERE poi_id=%(poi_id)s AND food_id=%(food_id)s",
+                     {"poi_id": poi_id, "food_id": food_id})
+
+    new_quantity = max(cur_quantity + quantity, 0)
+
+    with conn.cursor() as curs:
+        curs.execute("INSERT INTO poi_food (poi_id, food_id, quantity) VALUES(%(poi_id)s, %(food_id)s, %(quantity)s);",
+                     {"poi_id": poi_id, "food_id": food_id, "quantity": new_quantity})
